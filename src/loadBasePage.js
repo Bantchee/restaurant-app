dbceb91a7601b6d8e986c7d19ea5a00096a027d0
Jar'Zeno <jarzenojarrett@gmail.com>
2022-09-10 07:36:41 -0700

    Implment html for header

--- src/loadBasePage.js
+++ src/loadBasePage.js
@@ -11,22 +11,51 @@ export const loadBasePage = () => {
     
     // Header
     const header = document.createElement('header');
-    document.body.appendChild(header);
+    body.appendChild(header);
 
         // Title
+        const title = document.createElement('h1');
+        header.appendChild(title);
+        title.classList.add('title');
+        title.textContent = "Spring's Clover";
 
         // Nav
-            // Home
-            // Menu
-            // About
-            // Contact
+        const navbar = document.createElement('nav');
+        header.appendChild(navbar);
+            // a : Home
+            const homeLink = document.createElement('a');
+            navbar.appendChild(homeLink);
+            homeLink.classList.add('nav-link', 'current-page');
+            homeLink.setAttribute('href', '#');
+            homeLink.textContent = 'Home';
+
+            // a : Menu
+            const menuLink = document.createElement('a');
+            navbar.appendChild(menuLink);
+            menuLink.classList.add('nav-link');
+            menuLink.setAttribute('href', '#');
+            menuLink.textContent = 'Menu';
+
+            // a : About
+            const aboutLink = document.createElement('a');
+            navbar.appendChild(aboutLink);
+            aboutLink.classList.add('nav-link');
+            aboutLink.setAttribute('href', '#');
+            aboutLink.textContent = 'About';
+
+            // a : Contact
+            const contactLink = document.createElement('a');
+            navbar.appendChild(contactLink);
+            contactLink.classList.add('nav-link');
+            contactLink.setAttribute('href', '#');
+            contactLink.textContent = 'About';
 
     // Div : Content
     const content = document.createElement('div');
     content.setAttribute('id', 'content');
-    document.body.appendChild(content);
+    body.appendChild(content);
 
     // Footer
     const footer = document.createElement('footer');
-    document.body.appendChild(footer);
+    body.appendChild(footer);
 };
