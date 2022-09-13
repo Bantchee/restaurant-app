e3ad117ad8ae847c4cf5b7e5e34a8ed6009edc72
Jar'Zeno <jarzenojarrett@gmail.com>
2022-09-13 09:59:59 -0700

    Add basic slide show dynamic html

--- src/homePage.js
+++ src/homePage.js
@@ -4,11 +4,50 @@ export const homePage = () => {
     };
 
     const render = () => {
-        // Para : Cool
-        const para = document.createElement('p');
-        state.content.appendChild(para);
-        para.textContent = 'Cool';
-        para.style.color = "red";
+        slideShow();
+        // Div : Slide Show Container
+            // Div : Slide Container
+                // Div: Slides
+            // Div : Dot Container
+                // Span : Dots
+    };
+
+    const slideShow = () => {
+        // Div : Slide Show Container
+        const divSlideShow = document.createElement('div');
+        state.content.appendChild(divSlideShow);
+        state['slideShow'] = divSlideShow;
+        divSlideShow.classList.add('slide-show');
+
+        slideContainer();
+        dotContainer();
+    };
+
+    const slideContainer = () => {
+        // Div : Slide Container
+        const divSlideContainer = document.createElement('div');
+        state.slideShow.appendChild(divSlideContainer);
+        state['slideContainer'] = divSlideContainer;
+        divSlideContainer.classList.add('slide-container');
+
+            // Slide 1
+
+            // Slide 2
+
+            // Slide 3
+    };
+
+    const slide = (num, url) => {
+        const divSlide = document.createElement('div');
+        
+    };
+
+    const dotContainer = () => {
+        // Div : Slide Container
+        const divDotContainer = document.createElement('div');
+        state.slideShow.appendChild(divDotContainer);
+        state['dotContainer'] = divDotContainer;
+        divDotContainer.classList.add('dot-container');
     };
 
     return Object.assign({}, 
