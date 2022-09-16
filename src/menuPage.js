a38f7ccae9d0e4e22ed0d793ee558161da39a35e
Jar'Zeno <jarzenojarrett@gmail.com>
2022-09-16 16:31:22 -0700

    Implement basic menu modal for burger item

--- src/menuPage.js
+++ src/menuPage.js
@@ -7,6 +7,7 @@ import iceCreamImg from './img/ice-cream.jpg';
 export const menuPage = () => {
     const state = {
         content: document.querySelector('#content'),
+        modal: null,
     };
 
     const render = () => {
@@ -40,8 +41,38 @@ export const menuPage = () => {
         // Div : Modal
             // Div : Modal-Background-Tint
             // Div : Modal-Content
-        
+        createModal();
+
+        bindings();
     };
+
+    const bindings = () => {
+        // Get Menu Items
+        const burgerItem = document.querySelector('.burger');
+
+        // Bindings
+        burgerItem.addEventListener('click', () => {
+            changeModal('burger');
+            state.modal.style.display = 'flex';
+            exitBinding();
+        });
+    };
+
+    const exitBinding = () => {
+        // Get Menu Modal Exit
+        const exit = document.querySelector('.exit');
+
+        //Binding
+        exit.addEventListener('click', () => {
+            const modalContent = document.querySelector('.menu-modal-content');
+
+            while(modalContent.firstChild) {
+                modalContent.removeChild(modalContent.firstChild);
+            }
+
+            state.modal.style.display = 'none';
+        })
+    }
 
     const createMenuItem = (name, url) => {
         // Div : Item Container
@@ -60,6 +91,73 @@ export const menuPage = () => {
             paraItem.classList.add('text');
             paraItem.textContent = name[0].toUpperCase() + name.slice(1);
 
+    };
+
+    const createModal = () => {
+        // Div : Modal
+        const divModal = document.createElement('div');
+        document.body.appendChild(divModal);
+        state.modal = divModal;
+        divModal.classList.add('menu-modal');
+            // Div : Modal-Content
+            const modalContent = document.createElement('div');
+            divModal.appendChild(modalContent);
+            modalContent.classList.add('menu-modal-content');
+    };
+
+    // type is class name of menu item
+    const changeModal = (type) => {
+        // Get Div : Modal-Content
+        const modalContent = state.modal.querySelector('.menu-modal-content');
+            // Switch (type)
+            switch(type) {
+                case 'burger':
+                    // Exit
+                    const exitDiv = document.createElement('div');
+                    modalContent.appendChild(exitDiv);
+                    exitDiv.classList.add('exit-div');
+                        // Exit Text
+                        const exit = document.createElement('span');
+                        exitDiv.appendChild(exit);
+                        exit.classList.add('exit');
+                        exit.textContent = 'X';
+
+                    // Sizes
+                    const sizesTitle = document.createElement('p');
+                    modalContent.appendChild(sizesTitle);
+                    sizesTitle.textContent = "Burger Sizes :"
+                    sizesTitle.classList.add('title');
+                        // Ul
+                        const sizeList = document.createElement('ul');
+                        modalContent.appendChild(sizeList);
+                            // Small $5
+                            const smallSize = document.createElement('li');
+                            sizeList.appendChild(smallSize);
+                            smallSize.textContent = "Small $5";
+                            // Medium $10
+                            const mediumSize = document.createElement('li');
+                            sizeList.appendChild(mediumSize);
+                            mediumSize.textContent = "Medium $10";
+                            // Large $15
+                            const largeSize = document.createElement('li');
+                            sizeList.appendChild(largeSize);
+                            largeSize.textContent = "Large $15";
+            }
+                // Case : burger
+                    // Sizes and Prices
+                    // List Toppings
+                // Case : pizza
+                    // Sizes and Prices
+                    // List Toppings
+                // Case : fries
+                    // Sizes and Prices
+                    // List Seasonings
+                // Case : drinks
+                    // Sizes and Prices
+                    // List Flavors
+                // Case : ice-cream
+                    // Sizes and Prices
+                    // List Flavors
     };
 
     return Object.assign(
