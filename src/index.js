02283130cac392e06f18074f770af683a5ee28a5
Jar'Zeno <jarzenojarrett@gmail.com>
2022-09-16 11:10:25 -0700

    Add home.css import to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { loadBasePage } from './loadBasePage';
 import { homePage } from './homePage';
 import './style/index.css'
+import './style/home.css'
 
 loadBasePage();
 const hp = homePage();
